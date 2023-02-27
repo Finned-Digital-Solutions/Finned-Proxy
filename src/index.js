8ecf99c17e68025c6a93f9cc88164b7ddbb6f643
Finn Lancaster <finn.lancaster@finnsoftware.net>
2023-02-27 11:40:43 -0500

    Finish initial version, deploy to prod

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// simple cloudflare worker to request a url and return the response
+addEventListener('fetch', event => {
+	event.respondWith(handleRequest(event.request));
+});
+
+const isAuthenticated = (apiKey) => {
+	return apiKey === API_KEY;
+}
+
+const handleRequest = async (request) => {
+	// extract the url from the request
+	const url = new URL(request.url);
+	const auth = url.searchParams.get('auth');
+	if (!isAuthenticated(auth)) return new Response('unauthorized', { status: 403 });
+
+	const target = url.searchParams.get('url');
+	if (!target) return new Response('no url provided', { status: 400 });
+
+	// fetch the url
+	const response = await fetch(target, {
+		headers: {
+			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+			'Accept-Language': 'en-US,en;q=0.9',
+			'Accept-Encoding': 'gzip, deflate, br',
+			'Connection': 'keep-alive',
+			'Upgrade-Insecure-Requests': '1',
+		}
+	});
+
+	// return the response HTML
+	return new Response(response.body, {
+		status: response.status,
+		statusText: response.statusText,
+		headers: {
+			'Content-Type': 'text/html',
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+			'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
+		}
+	});
+}
